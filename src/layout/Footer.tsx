62ad159ad2f3ebe7ee3aa57e5670795c7f3258ad
jieeeun2 <jieun2.dev@gmail.com>
2024-01-08 02:01:10 +0900

    design: Footer 구현

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return <FooterLayout>Copyright 2024. Jieeeun All rights reserved.</FooterLayout>
 }
 
 export default Footer
+
+const FooterLayout = styled.div`
+  background: ${({ theme }) => theme.background.alt};
+  color: ${({ theme }) => theme.neutral.dark};
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+`
