2aece161db426c149dec472daabde7aa081367b0
jieeeun2 <jieun2.dev@gmail.com>
2024-01-10 14:46:00 +0900

    feat: 포스팅위젯 UI 구현 및 재사용컴포넌트 추가 생성

--- src/components/home/PostingWidget.tsx
+++ src/components/home/PostingWidget.tsx
@@ -1,7 +1,92 @@
-import React from 'react'
+import styled from 'styled-components'
+import {
+  MdOutlineImage,
+  MdOutlineVideoCameraBack,
+  MdOutlineAttachFile,
+  MdOutlineMicNone,
+} from 'react-icons/md'
+import {
+  Button,
+  FlexBetween,
+  Hr,
+  IconButton,
+  Textarea,
+  WidgetLayout,
+} from 'styles/ReuseableComponent'
 
 const PostingWidget = () => {
-  return <div>PostingWidget</div>
+  return (
+    <PostingWidgetLayout>
+      <ContentBox>
+        <img src='/images/profileImage.jpg' />
+        <Textarea placeholder='오늘 무슨일이 있었나요!' spellCheck='false' />
+      </ContentBox>
+      <Hr />
+      <AttachmentAndSubmitBox>
+        <AttachmentSection>
+          <IconButton>
+            <MdOutlineImage className='icon' />
+            <span>이미지</span>
+          </IconButton>
+          <IconButton>
+            <MdOutlineVideoCameraBack className='icon' />
+            <span>동영상</span>
+          </IconButton>
+          <IconButton>
+            <MdOutlineAttachFile className='icon' />
+            <span>첨부파일</span>
+          </IconButton>
+          <IconButton>
+            <MdOutlineMicNone className='icon' />
+            <span>음성녹음</span>
+          </IconButton>
+        </AttachmentSection>
+        <Button>등록</Button>
+      </AttachmentAndSubmitBox>
+    </PostingWidgetLayout>
+  )
 }
 
 export default PostingWidget
+
+const PostingWidgetLayout = styled(WidgetLayout)`
+  width: 500px;
+  box-sizing: border-box;
+
+  & > div {
+    margin: 12px;
+  }
+
+  .icon {
+    font-size: 24px;
+  }
+`
+
+const ContentBox = styled(FlexBetween)`
+  gap: 12px;
+
+  & > img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-top: 0;
+    margin-bottom: auto;
+  }
+`
+
+const AttachmentAndSubmitBox = styled(FlexBetween)`
+  & > button {
+    height: 36px;
+    background: ${({ theme }) => theme.primary.light};
+    color: ${({ theme }) => theme.primary.main};
+  }
+`
+
+const AttachmentSection = styled.div`
+  display: flex;
+  gap: 12px;
+
+  & > button {
+    height: 36px;
+  }
+`
--- src/styles/ReuseableComponent.tsx
+++ src/styles/ReuseableComponent.tsx
@@ -9,28 +9,75 @@ export const FlexBetween = styled.div`
 export const Input = styled.input`
   background: transparent;
   border: none;
+  border-radius: 8px;
   background: ${({ theme }) => theme.neutral.light};
   color: ${({ theme }) => theme.neutral.dark};
   outline: none;
   width: 100%;
+  min-height: 40px;
+  padding: 0 12px;
 `
 
-export const IconButton = styled.button`
-  cursor: pointer;
+export const Textarea = styled.textarea`
   background: transparent;
   border: none;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.neutral.light};
+  color: ${({ theme }) => theme.neutral.dark};
+  outline: none;
+  width: 100%;
+  height: 80px;
+  padding: 12px;
+  resize: none;
 `
 
-export const Button = styled.button``
+export const Button = styled.button`
+  cursor: pointer;
+  background: ${({ theme }) => theme.neutral.light};
+  color: ${({ theme }) => theme.neutral.dark};
+  width: 60px;
+  border: none;
+  border-radius: 8px;
+  padding: 8px 12px;
+  margin: 0;
+
+  &:hover {
+    background: ${({ theme }) => theme.primary.main};
+    color: ${({ theme }) => theme.background.default};
+  }
+`
+
+export const IconButton = styled(Button)`
+  width: fit-content;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  gap: 2px;
+
+  & > span {
+    font-size: 12px;
+  }
+`
 
 export const WidgetLayout = styled.div`
   padding: 8px;
   background: ${({ theme }) => theme.background.alt};
+  color: ${({ theme }) => theme.neutral.dark};
   border-radius: 8px;
 `
 
 export const Hr = styled.hr`
-  border: 0 solid ${({ theme }) => theme.neutral.medium};
+  border: 0 solid rgba(255, 255, 255, 0.2);
   border-bottom-width: thin;
   margin: 0 12px;
+`
+
+export const Span = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.neutral.mediumMain};
+
+  &.bold {
+    font-weight: 700;
+  }
 `
