2c8532ab130717460db904a91025ffb35a0836cc
jieeeun2 <jieun2.dev@gmail.com>
2024-01-15 15:53:34 +0900

    feat: axios인스턴스 생성 및 응답인터셉터 구현

---
+++ src/configs/axiosInstance.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosError } from 'axios'
+
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3001',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    const formattedMessage = `success ${response.data.message}`
+
+    return {
+      ...response,
+      data: {
+        ...response.data,
+        message: formattedMessage,
+      },
+    }
+  },
+  (error: AxiosError | Error): Promise<AxiosError> => {
+    if (axios.isAxiosError(error) && error.response) {
+      const statusCode = error.response.status
+      const statusText = error.response.statusText
+      const message = error.response.data.message
+      console.log(`${statusCode} (${statusText}) ${message}`)
+
+      const formattedMessage = `fail ${message}`
+
+      return Promise.reject({
+        ...error,
+        response: {
+          ...error.response,
+          data: {
+            ...error.response.data,
+            message: formattedMessage,
+          },
+        },
+      })
+    } else {
+      return Promise.reject(error)
+    }
+  },
+)
