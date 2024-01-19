ad14fd89df08d4379d52eb02e3051c97041361f4
jieeeun2 <jieun2.dev@gmail.com>
2024-01-20 00:05:00 +0900

    feat: 로그인, 회원가입 api호출 구현 및 임시 적용 (+)
    - 이렇게 한 이유는 로그인, 회원가입 구현 관련 (UI, 유효성검사, 자동로그인, 엑세스/리프레시토큰)은 많이 해봐서 흥미가 없어서... 다른 부분 먼저하고 하려고함
    - post 쪽에서 전역 저장되어있는 사용자정보가 필요하기에 이렇게 임시로 만들어두었음

---
+++ src/apis/authApi.ts
@@ -0,0 +1,49 @@
+import { axiosInstance } from 'configs/axiosInstance'
+
+interface RegisterData {
+  name: string
+  email: string
+  password: string
+  profileImage: File
+  location: string
+  occupation: string
+}
+
+interface LoginData {
+  email: string
+  password: string
+  isAutoLogin: boolean
+}
+
+export const authApi = {
+  async register({ name, email, password, profileImage, location, occupation }: RegisterData) {
+    try {
+      const registerInputValue = { name, email, password, profileImage, location, occupation }
+      const registerInputValueList = Object.entries(registerInputValue)
+
+      const formData = new FormData()
+      registerInputValueList.map((value) => formData.append(value[0], value[1]))
+
+      const response = await axiosInstance('/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        data: formData,
+      })
+      return response.data
+    } catch (err) {
+      console.log('err', err)
+    }
+  },
+
+  async login({ email, password, isAutoLogin }: LoginData) {
+    try {
+      const response = await axiosInstance('/auth/login', {
+        method: 'POST',
+        data: { email, password, isAutoLogin },
+      })
+      return response.data
+    } catch (err) {
+      console.log('err', err)
+    }
+  },
+}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,45 @@
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
+import { authApi } from 'apis/authApi'
+import { setUser } from 'store/userSlice'
+import { setAccessToken } from 'utils/localStorage'
 
 const LoginPage = () => {
-  return <LoginPageLayout>LoginPage</LoginPageLayout>
+  const dispatch = useDispatch()
+
+  const temporaryRegister = async () => {
+    const mockData = {
+      name: 'test2',
+      email: 'test2@sociopedia.com',
+      password: 'test2',
+      profileImage: new File(['dummy content'], 'dummy_filename.jpg', { type: 'image/jpeg' }),
+      location: '',
+      occupation: '',
+    }
+    const registerResult = await authApi.register(mockData)
+    console.log('registerResult', registerResult)
+  }
+
+  const temporaryLogin = async () => {
+    const isAutoLogin = false
+    const mockData = {
+      email: 'test2@sociopedia.com',
+      password: 'test2',
+      isAutoLogin,
+    }
+    const loginResult = await authApi.login(mockData)
+    const { accessToken, user } = loginResult.data
+
+    if (!isAutoLogin) setAccessToken(accessToken)
+    dispatch(setUser({ user: user }))
+  }
+
+  return (
+    <LoginPageLayout>
+      <button onClick={temporaryRegister}>temporaryRegister</button>
+      <button onClick={temporaryLogin}>temporaryLogin</button>
+    </LoginPageLayout>
+  )
 }
 
 export default LoginPage
