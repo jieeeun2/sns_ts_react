b8b70661d9fe12cd16a77debd4113a63b420afd0
jieeeun2 <jieun2.dev@gmail.com>
2024-01-21 13:10:21 +0900

    feat: redux-toolkit 설정에 redux-persist 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { RouterProvider } from 'react-router-dom'
-import { store } from 'store'
+import { persistor, store } from 'store'
 import StyleProvider from 'styles/StyleProvider'
 import { routers } from 'routes/routers'
 
@@ -10,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <StyleProvider>
-        <RouterProvider router={routers} />
-      </StyleProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <StyleProvider>
+          <RouterProvider router={routers} />
+        </StyleProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import modeReducer from 'store/modeSlice'
 import userReducer from 'store/userSlice'
 
-export const store = configureStore({
-  reducer: {
-    mode: modeReducer,
-    user: userReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({
+  mode: modeReducer,
+  user: userReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
