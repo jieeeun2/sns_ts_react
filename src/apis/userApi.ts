b4ef676210c7d797fb96bb4751563d0f5d824b43
jieeeun2 <jieun2.dev@gmail.com>
2024-03-28 16:30:50 +0900

    feat: 팔로우(팔로잉+팔로워)목록 조회 api 구현

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,9 +1,21 @@
 import { axiosInstance } from 'configs/axiosInstance'
-import { UpdateFollowingList, GetUserInfo } from 'types/userApiType'
+import { UpdateFollowingList, GetUserInfo, GetFollowList } from 'types/userApiType'
 
 export const getUserInfo = async ({ userId }: GetUserInfo) => {
   try {
     const response = await axiosInstance(`/user/${userId}`, {
+      method: 'GET',
+    })
+
+    return response.data.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getFollowList = async ({ userId }: GetFollowList) => {
+  try {
+    const response = await axiosInstance(`/user/${userId}/follows`, {
       method: 'GET',
     })
 
--- src/types/userApiType.ts
+++ src/types/userApiType.ts
@@ -2,6 +2,10 @@ export interface GetUserInfo {
   userId: string
 }
 
+export interface GetFollowList {
+  userId: string
+}
+
 export interface UpdateFollowingList {
   userId: string
   targetUserId: string
