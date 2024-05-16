975f40493b3daa0533e2b182265395832a335b88
jieeeun2 <jieun2.dev@gmail.com>
2024-05-16 15:39:58 +0900

    feat: axiosInstance의 baseUrl에 환경변수값 받아서 사용하도록 추가

--- src/configs/axiosInstance.ts
+++ src/configs/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios /* , { AxiosError }  */ from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.BASE_URL || 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
   },
